4ec8e5e12f714a2866250c0f319ac7b999fdfe9d
Dave P <65463983+d4ve-p@users.noreply.github.com>
2024-03-07 08:27:40 +0700

    added from_string for network device type

--- src/models/__network_device_type.py
+++ src/models/__network_device_type.py
@@ -10,3 +10,23 @@ class NetworkDeviceType(Enum):
     CISCO_IOS_XE = 'cisco_xe'
     MIKROTIK_SWITCH_OS = 'mikrotik_switchos'
     MIKROTIK_ROUTER_OS = 'mikrotik_routeros'
+
+    @staticmethod
+    def from_string(string: str) -> 'NetworkDeviceType':
+        """
+        Converts string to enum
+        """
+
+        string = string.lower()
+
+        translators = {
+            "cisco ios xe": NetworkDeviceType.CISCO_IOS_XE,
+            "cisco ios": NetworkDeviceType.CISCO_IOS,
+            "mikrotik switchos": NetworkDeviceType.MIKROTIK_SWITCH_OS,
+            "mikrotik routeros": NetworkDeviceType.MIKROTIK_ROUTER_OS,
+        }
+
+        if translators.get(string) is None:
+            raise ValueError(f"device type \"{string}\" not found. Available keys: {", ".join([i for i in translators.keys()])}")
+
+        return translators[string]
